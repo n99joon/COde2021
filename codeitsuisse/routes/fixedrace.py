b3596eff75c1bb4e710be3263d2fe1699bb90bae
n99joon <52797682+n99joon@users.noreply.github.com>
2021-09-25 19:48:27 +0900

    Create fixedrace.py

---
+++ codeitsuisse/routes/fixedrace.py
@@ -0,0 +1,26 @@
+import logging
+import json
+import copy
+from decimal import Decimal
+import math
+
+from flask import request, jsonify
+
+from codeitsuisse import app
+
+logger = logging.getLogger(__name__)
+
+xmov = [1,0,-1,0,1,-1,-1,1]
+ymov = [0,1,0,-1,1,1,-1,-1]
+
+
+@app.route('/fixedrace', methods=['POST'])
+def fixedr():
+  ret =[]
+  data = request.get_json()
+  logging.info("data sent for evaluation {}".format(data))
+  tc = data.get("test_cases")
+ 
+  ret.append({"input": test, "score": maxp, "origin": maxi})
+  logging.info("My result :{}".format(ret))
+  return json.dumps(ret)
