99239e025eaba3e8b457360b962a2c1bf13df866
n99joon <52797682+n99joon@users.noreply.github.com>
2021-09-25 18:23:00 +0900

    Update asteroid.py

--- codeitsuisse/routes/asteroid.py
+++ codeitsuisse/routes/asteroid.py
@@ -23,21 +23,24 @@ def aster():
     maxi=0
     #moving middle points
     for k in range(len(test)):
+      logging.info("k {}".format(k))
       kp =0.0
       if (k==0 or k==len(test)-1):
         continue
       stack=[]
       for i in range(k):
         stack.append(test[i])
+      logging.info("stack {}".format(stack))
       j = k+1
       while j<len(test) and len(stack)>0:
         curp = 0
-        if(j==k+1 and test[k]==test[j]):
+        if(j==k+1 && test[k]==test[j]):
           curp+=1
         else:
           kp+=1
         #doesn't match
         if(test[j]!=stack[-1]):
+          logging.info("doesnt match: kp & maxi & maxp {}".format([kp,maxi,maxp]))
           if(curp<=6):
             kp+=curp
           elif(curp<=10):
@@ -62,6 +65,7 @@ def aster():
           kp+=1.5*curp
         else:
             kp+=curp*2
+      logging.info("reached end of mid point kp {}".format([kp]))
       if(kp>maxp):
         maxi=k
         maxp=kp
