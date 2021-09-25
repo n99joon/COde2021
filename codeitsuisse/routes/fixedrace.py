14dda8a4cbdb56042ee2f896c6f450385830411a
n99joon <52797682+n99joon@users.noreply.github.com>
2021-09-25 22:30:11 +0900

    Update fixedrace.py

--- codeitsuisse/routes/fixedrace.py
+++ codeitsuisse/routes/fixedrace.py
@@ -10,16 +10,72 @@ from codeitsuisse import app
 
 logger = logging.getLogger(__name__)
 
-xmov = [1,0,-1,0,1,-1,-1,1]
-ymov = [0,1,0,-1,1,1,-1,-1]
+n=0
+dict={}
+edgePointed=[]
+edgePointTo=[]
+sortedL = []
+inpsplit = []
+
+def myinput(winner, inpsplit):
+  winnerkey = dict.get(winner)
+  for t in inpsplit:
+    if t!=winner:
+      loserkey = dict.get(t)
+      edgePointed[loserkey].append(winnerkey)
+      edgePointTo[winnerkey].append(loserkey)
+  
+  queue = []
+  #Calcualte the sorted array with Kuhn algorithm(ts)
+  #find nodes not being pointed
+  for m in dict:
+    if len(edgePointed[dict.get(m)])==1:
+      queue.append(m)
+  sortedL=[]
+  while len(queue)>0:
+    front = queue.pop(0)
+    frontkey = dict.get(front)
+    sortedL.append(front)
+    for pp in edgePointTo[frontkey]:
+      if pp == -1:
+        continue
+      edgePointed[pp].remove(frontkey)
+      edgePointTo[frontkey].remove(pp)
+      if len(edgePointed[pp]) == 1:
+        ppkey=" "
+        for g in dict:
+          if dict.get(g)==pp:
+            ppkey=g
+            break
+        queue.append(ppkey)
+  logging.info("My result :{}".format(sortedL))
+  return sortedL
 
 
 @app.route('/fixedrace', methods=['POST'])
 def fixedr():
   ret =[]
-  for x in range(5000):
-    data=request.data
-    logging.info("data sent for evaluation {}".format(data))
- 
-  logging.info("My result :{}".format(ret))
-  return json.dumps(ret)
+  data=request.data
+  logging.info("data sent for evaluation {}".format(data))
+  if(data[0]=="0"):
+    winner = data[1:]
+    myinput(winner)
+  else:
+    inp =data
+    ans=""
+    inpsplit = inp.split(,)
+    for a in inpsplit:
+      if not a in dict:
+        dict[a]=n
+        edgePointed.append([-1])
+        edgePointTo.append([-1])
+        n+=1
+    for b in sortedL:
+      if b in inpsplit:
+        ans+=b
+        ans+=","
+    ans=ans[:-1]
+    logging.info("My result :{}".format(ans))
+    return ans
+
+
