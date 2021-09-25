5891256bac85e1ac1812b5d3d2711c4ed9d517bc
n99joon <52797682+n99joon@users.noreply.github.com>
2021-09-25 14:41:07 +0900

    Update parasite.py

--- codeitsuisse/routes/parasite.py
+++ codeitsuisse/routes/parasite.py
@@ -54,7 +54,7 @@ def bfsthree(grid,newGrid, i, j):
       v = queue.pop(0)
       curi = v[0]
       curj = v[1]
-      for k in range(4):
+      for k in range(8):
         ni = curi+ymov[k]
         nj = curj+xmov[k]
         if (ni>=0 and ni<i and nj>=0 and nj<j):
@@ -64,7 +64,7 @@ def bfsthree(grid,newGrid, i, j):
             grid[ni][nj]=3;
             queue.append([ni,nj])
     newGrid[start[0]][start[1]]=-1
-    return [newGrid,grid,timeTaken]    
+    return [grid,timeTaken]    
             
 
 @app.route('/parasite', methods=['POST'])
@@ -80,7 +80,7 @@ def para():
       indiv = dt.get("interestedIndividuals")
       newGrid = [[-1 for x in range(j)] for y in range (i)]
       newL = bfs(grid,newGrid, i, j)
-      newGrid = newL[0]
+      newGrido = newL[0]
       infectedGrid = newL[1]
       tt = newL[2]
 
@@ -89,14 +89,23 @@ def para():
         idx = k.find(',')
         ni = int(k[:idx])
         nj = int(k[idx+1:])
-        pOne[k]=newGrid[ni][nj]
+        pOne[k]=newGrido[ni][nj]
       
+
       for x in range (i):
         for y in range (j):
           if infectedGrid[x][y]==1:
             tt = -1
             break
 
-      ret.append({"room": room, "p1": pOne, "p2": tt, "p3": 0, "p4": 0})
+      ThrL = bfs(grid,newGrid,i,j)
+      infectedG = ThrL[0]
+      ttt = Thr[1]
+      for x in range (i):
+        for y in range (j):
+          if infectedG[x][y]==1:
+            ttt = -1
+            break
+      ret.append({"room": room, "p1": pOne, "p2": tt, "p3": ttt, "p4": 0})
       logging.info("My result :{}".format(ret))
     return json.dumps(ret)
