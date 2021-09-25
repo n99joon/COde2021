982e30b666cd8b0e1bfca342c8c1e4fee8761316
n99joon <52797682+n99joon@users.noreply.github.com>
2021-09-25 19:55:02 +0900

    Update fixedrace.py

--- codeitsuisse/routes/fixedrace.py
+++ codeitsuisse/routes/fixedrace.py
@@ -14,7 +14,7 @@ xmov = [1,0,-1,0,1,-1,-1,1]
 ymov = [0,1,0,-1,1,1,-1,-1]
 
 
-@app.route('/fixedrace', methods=['POST'])
+@app.route('/fixedrace', methods=['GET'])
 def fixedr():
   ret =[]
   data = request.get_json()
