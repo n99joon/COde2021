571f7b2beeec52b2c689359569f787c9e3c652d1
n99joon <52797682+n99joon@users.noreply.github.com>
2021-09-25 14:33:01 +0900

    Update parasite.py

--- codeitsuisse/routes/parasite.py
+++ codeitsuisse/routes/parasite.py
@@ -7,8 +7,8 @@ from codeitsuisse import app
 
 logger = logging.getLogger(__name__)
 
-xmov = [1,0,-1,0]
-ymov = [0,1,0,-1]
+xmov = [1,0,-1,0,1,-1,-1,1]
+ymov = [0,1,0,-1,1,1,-1,-1]
 
 def bfs(grid,newGrid, i, j):
     timeTaken=0
@@ -38,7 +38,33 @@ def bfs(grid,newGrid, i, j):
     newGrid[start[0]][start[1]]=-1
     return [newGrid,grid,timeTaken]
     
-    
+def bfsthree(grid,newGrid, i, j):
+    timeTaken=0
+    logging.info("i,j : {}".format([i,j]))
+    for x in range(i):
+        for y in range(j):
+            logging.info("x : {}".format(x))
+            logging.info("y : {}".format(y))
+            if grid[x][y]==3:
+                start = [x,y];
+                newGrid[x][y]=0
+                break
+    queue = [start]
+    while queue:
+      v = queue.pop(0)
+      curi = v[0]
+      curj = v[1]
+      for k in range(4):
+        ni = curi+ymov[k]
+        nj = curj+xmov[k]
+        if (ni>=0 and ni<i and nj>=0 and nj<j):
+          if(grid[ni][nj]==1):
+            newGrid[ni][nj]=newGrid[curi][curj]+1;
+            timeTaken=max(newGrid[ni][nj],timeTaken)
+            grid[ni][nj]=3;
+            queue.append([ni,nj])
+    newGrid[start[0]][start[1]]=-1
+    return [newGrid,grid,timeTaken]    
             
 
 @app.route('/parasite', methods=['POST'])
@@ -49,8 +75,8 @@ def para():
     for dt in data:
       room = dt.get("room")
       grid = dt.get("grid")
-      j = len(grid)
-      i = len(grid[0])
+      i = len(grid)
+      j = len(grid[0])
       indiv = dt.get("interestedIndividuals")
       newGrid = [[-1 for x in range(j)] for y in range (i)]
       newL = bfs(grid,newGrid, i, j)
