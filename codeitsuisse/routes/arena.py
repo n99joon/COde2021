aa746ef0a028394a91ce95ca08bf19eee97f219b
n99joon <52797682+n99joon@users.noreply.github.com>
2021-09-25 12:35:11 +0900

    Update arena.py

--- codeitsuisse/routes/arena.py
+++ codeitsuisse/routes/arena.py
@@ -7,7 +7,7 @@ from codeitsuisse import app
 
 logger = logging.getLogger(__name__)
 
-@app.route('/tictactoe', methods=['POST'])
+@app.route('/tic-tac-toe', methods=['POST'])
 def ttt():
     data = request.get_json()
     logging.info("data sent for evaluation {}".format(data))
