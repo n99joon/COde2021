e71e50c37424d1217334a660f7eb888df296bfbd
n99joon <52797682+n99joon@users.noreply.github.com>
2021-09-25 12:28:03 +0900

    Update tic-tac-toe.py

--- codeitsuisse/routes/tic-tac-toe.py
+++ codeitsuisse/routes/tic-tac-toe.py
@@ -7,7 +7,7 @@ from codeitsuisse import app
 
 logger = logging.getLogger(__name__)
 
-@app.route('/square', methods=['POST'])
+@app.route('/tic-tac-toe', methods=['POST'])
 def evaluate():
     data = request.get_json()
     logging.info("data sent for evaluation {}".format(data))
