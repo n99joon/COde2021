e10ab91d867b9b45c7e2a015c3ce2baf5797f6ec
n99joon <52797682+n99joon@users.noreply.github.com>
2021-09-26 09:40:03 +0900

    Update __init__.py

--- codeitsuisse/__init__.py
+++ codeitsuisse/__init__.py
@@ -1,10 +1,10 @@
 from flask import Flask
 app = Flask(__name__)
-import codeitsuisse.routes.square
+#import codeitsuisse.routes.square
 
-import codeitsuisse.routes.arena
-import codeitsuisse.routes.parasite
-import codeitsuisse.routes.asteroid
-import codeitsuisse.routes.fixedrace
-import codeitsuisse.routes.frc
+#import codeitsuisse.routes.arena
+#import codeitsuisse.routes.parasite
+#import codeitsuisse.routes.asteroid
+#import codeitsuisse.routes.fixedrace
+#import codeitsuisse.routes.frc
 import codeitsuisse.routes.decoder
