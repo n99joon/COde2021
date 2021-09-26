72db470136cca5c2d2fb2fdd1bd81b67392e1a76
n99joon <52797682+n99joon@users.noreply.github.com>
2021-09-26 10:17:58 +0900

    Update decoder.py

--- codeitsuisse/routes/decoder.py
+++ codeitsuisse/routes/decoder.py
@@ -20,7 +20,7 @@ def decoder():
   pv = data.get("possible_values")
   num = data.get("num_slots")
   history = data.get("history")
-  ans = ['s','s','k','e','k']
+  ans = ['a','a','b','k','b']
   ret={"answer": ans}
   logging.info("ret {}".format(ret))
   return json.dumps(ret)
